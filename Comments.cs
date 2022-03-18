c93bc7db9ba9a87efe5451bfa51b5876842c055f
hadi sabounchi <hadisabounchi@gmail.com>
2022-03-18 20:53:56 +0330

    add entities classes in project

---
+++ Comments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_test_001130
+{
+    public class Comments
+    {
+        [Key] public int CommentID { get; set; }
+        public int ViewerID { get; set; }
+        public string Context { get; set; }
+        public int ProductID { get; set; }
+    }
+}
---
+++ Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_test_001130
+{
+    public class Products
+    {
+        [Key] public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int ProductCost { get; set; }
+        public string ProductImage { get; set; }
+    }
+}
